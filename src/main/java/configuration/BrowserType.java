b1a973d01a7ee48095565802a4ce852ad3cddcfa
newqa <mpancewiczpuchalska@gmail.com>
2022-06-26 23:28:17 +0200

    Corrected formatting

--- src/main/java/configuration/BrowserType.java
+++ src/main/java/configuration/BrowserType.java
@@ -1,9 +1,12 @@
 package configuration;
 
 public enum BrowserType {
+
     CHROME("chrome"),
     EDGE("edge");
+
     private final String browser;
+
     BrowserType(String browser) {
         this.browser = browser;
     }
