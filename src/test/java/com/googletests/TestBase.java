2e4521fcb49879aa0fa69a3d1925408a9312f304
newqa <mpancewiczpuchalska@gmail.com>
2022-06-27 09:42:37 +0200

    removed .idea and .allure folders

--- src/test/java/com/googletests/TestBase.java
+++ src/test/java/com/googletests/TestBase.java
@@ -4,12 +4,8 @@ import configuration.DriverManager;
 import configuration.DriverUtils;
 import configuration.Settings;
 import org.aeonbits.owner.ConfigFactory;
-import org.apache.log4j.BasicConfigurator;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-
-import java.util.Map;
 
 public class TestBase {
     public static Settings settings = ConfigFactory.create(Settings.class, System.getProperties());
